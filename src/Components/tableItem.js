a8263d1814c80585b93f8780674ee39598552382
arun7sun <arunkumarcr7ak@gmail.com>
2017-11-09 18:22:15 +0530

    Updated table with data from fake API

---
+++ src/Components/tableItem.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+
+class TableItem extends Component {
+  render() {
+    
+    return (
+    <div className="TableItem">
+        <table>
+            <tbody>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>EMAIL</th>
+                    <th>Company</th>
+                    <th>CITY</th>
+                </tr>
+                <tr >
+                    <td >{this.props.user.id}</td>
+                    <td>{this.props.user.name}</td>
+                    <td>{this.props.user.email}</td>
+                    <td>{this.props.user.company.name}</td>
+                    <td>{this.props.user.address.city}</td>
+                </tr>
+        
+            </tbody>
+        </table>
+    </div>
+    );
+  }
+}
+
+export default TableItem;
