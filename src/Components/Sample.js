8203f1217531bf32b680652a189387b0cfbc64d6
arun7sun <arunkumarcr7ak@gmail.com>
2017-12-26 16:36:46 +0530

    redux

--- src/Components/Sample.js
+++ src/Components/Sample.js
@@ -1,10 +1,20 @@
-import { createStore } from "redux";
+import { createStore } from 'redux';
 import React, { Component } from 'react';
 
 class Sample extends Component {
+    constructor(){
+        super();
+        let counter = (state =0,action) => 
+        {
+         switch(action.type){
+            case "INCREMENT": return state +1;
+            case "Decrement": return state -1;
+            default : return state;}
+        };
+    }
  render(){
-    let store = createStore;
-    return state;
- }
+    let store = createStore(this.counter);
+    return store.getState();
+  }
 }
 export default Sample;
