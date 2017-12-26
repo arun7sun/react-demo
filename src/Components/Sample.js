f479b5574524ea7890311ba419d059e60efe766d
arun7sun <arunkumarcr7ak@gmail.com>
2017-12-26 16:43:16 +0530

    Wokring state prj

--- src/Components/Sample.js
+++ src/Components/Sample.js
@@ -1,20 +1,21 @@
 import { createStore } from 'redux';
-import React, { Component } from 'react';
+import { Component } from 'react';
 
 class Sample extends Component {
     constructor(){
         super();
-        let counter = (state =0,action) => 
-        {
-         switch(action.type){
-            case "INCREMENT": return state +1;
-            case "Decrement": return state -1;
-            default : return state;}
-        };
+        // let counter = (state =0,action) => 
+        // {
+        //  switch(action.type){
+        //     case "INCREMENT": return state +1;
+        //     case "Decrement": return state -1;
+        //     default : return state;}
+        // };
+        // let store = createStore(this.counter);
     }
  render(){
-    let store = createStore(this.counter);
-    return store.getState();
+    // return this.store.getState();
+    return 0;
   }
 }
 export default Sample;
